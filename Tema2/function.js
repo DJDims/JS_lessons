8fd4416f167d9dde6fe6aa45d78031e099a4f077
pupil <pupil@ivkhk.ee>
2022-09-05 12:52:11 +0300

    Update

--- Tema2/function.js
+++ Tema2/function.js
@@ -1,40 +1,49 @@
 function showGroupData(){
     let groupCode = document.getElementById('groupCode').value;
+    groupCode = groupCode.toUpperCase();
     if (groupCode.length != 7) {
-        showError('Введите коректный код группы')
+        showError('Введите коректный код группы');
+        return;
     }
+    
+    document.getElementById('group').style.display = 'block';
+    document.getElementById('group').innerHTML = groupCode;
+
     let city = groupCode.substr(0,1);
-    if (city == 'N' || city == 'n') {
+    if (city == 'N') {
         document.getElementById('city').innerHTML = 'Город: Нарва';
-    } else if (city == 'S' || city == 's') {
+    } else if (city == 'S') {
         document.getElementById('city').innerHTML = 'Город: Силламяэ';
-    } else if (city == 'J' || city == 'j') {
+    } else if (city == 'J') {
         document.getElementById('city').innerHTML = 'Город: Йыхви';
     } else {
         showError('Указан неверный город')
         return;
     }
+    document.getElementById('city').style.display = 'block';
 
     let school = groupCode.substr(1,1);
-    if (school == 'P' || school == 'p') {
+    if (school == 'P') {
         document.getElementById('school').innerHTML = 'После 9го класса';
-    } else if (school == 'K' || school == 'k') {
+    } else if (school == 'K') {
         document.getElementById('school').innerHTML = 'После 12го класса';
     } else {
         showError('Указан неверная школа');
         return;
     }
+    document.getElementById('school').style.display = 'block';
 
     let language = groupCode.substr(4,1);
     console.log(language)
-    if (language == 'R' || language == 'r') {
+    if (language == 'R') {
         document.getElementById('language').innerHTML = 'Русская группа';
-    } else if (language == 'E' || language == 'e') {
+    } else if (language == 'E') {
         document.getElementById('language').innerHTML = 'Эстонская группа';
     } else {
         showError('Указан неверный язык обучения');
         return;
     }
+    document.getElementById('language').style.display = 'block';
 
     let year = groupCode.substr(5,2);
     if (year == 20) {
@@ -47,9 +56,15 @@ function showGroupData(){
         showError('Указан неверный год поступления');
         return;
     }
+    document.getElementById('year').style.display = 'block';
 
 }
 
 function showError(text) {
-    alert(text)
+    document.getElementById('group').style.display = 'none';
+    document.getElementById('city').style.display = 'none';
+    document.getElementById('school').style.display = 'none ';
+    document.getElementById('language').style.display = 'none';
+    document.getElementById('year').style.display = 'none';
+    alert(text);
 }
